e8cec1f566a63b4d37edd24b256d18707dab3e1b
Nikita <Avlasenko.nikitaa@gmail.com>
2020-06-03 21:43:33 +0300

    Add files via upload

---
+++ famdata.js
@@ -0,0 +1,200 @@
+﻿var famdata = [
+    
+  
+
+
+
+
+/* 
+
+
+                                                              id: 29 - Everyone must have v unique id
+
+                                                              title — Surname Name
+
+                                                              description
+
+                                                              image: «photos/y.png» — photo
+
+                                                              itemTitleColor: «#ffc0cb» — Header color
+
+                                                              parents: [3, 4] — id of parents is indicated, if it is v parent this parameter is not specified.
+
+    
+
+    */
+
+
+
+
+
+///////////////////////////////////////////                                generation -1                                  /////////////////////////////////////
+
+
+  { id: 1, title:  "Surname Name", description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe",},
+  { id: 101, title: "Surname Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+ 
+
+
+
+
+
+
+///////////////////////////////////////////                                generation -2                                  ///////////////////////////////////// 
+
+
+  { id: 2, parents: [1, 101], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 102, title: "Surname Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+
+  { id: 3, parents: [1, 101], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 103, title: "Surname Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+
+  { id: 4, parents: [1, 101], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 104, title: "Surname Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+
+  { id: 5, parents: [1, 101], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 105, title: "Surname Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+
+
+
+
+
+
+
+///////////////////////////////////////////                                generation -3                                  ///////////////////////////////////// 
+
+
+  { id: 6, parents: [2, 102], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 106, title: "Surname Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+
+  { id: 7, parents: [2, 102], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 107,  title: "Sername Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+
+  { id: 8, parents: [2, 102], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 108,  title: "Sername Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+
+  { id: 9, parents: [2, 102], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 109,  title: "Sername Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+
+
+  { id: 10, parents: [3, 103], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 110, title: "Suename Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+
+  { id: 11, parents: [3, 103], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 111, title: "Suename Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+
+  { id: 12, parents: [3, 103], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 112, title: "Suename Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+
+  { id: 13, parents: [3, 103], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 113, title: "Suename Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+
+
+  { id: 14, parents: [4, 104], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 114, title: "Suename Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+
+  { id: 15, parents: [4, 104], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 115, title: "Suename Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+
+  { id: 16, parents: [4, 104], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 116, title: "Suename Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+
+  { id: 17, parents: [4, 104], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 117, title: "Suename Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+
+
+  { id: 18, parents: [5, 105], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 118, title: "Suename Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+
+  { id: 19, parents: [5, 105], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 119, title: "Suename Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+
+  { id: 20, parents: [5, 105], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 120, title: "Suename Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" },
+
+  { id: 21, parents: [5, 105], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 121, title: "Suename Name",  description: "Description", image: "photos/y.png", itemTitleColor: "#ffc0cb" }, 
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////                                generation -4                                  /////////////////////////////////////  
+
+
+  { id: 22, parents: [6, 106], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },    
+/*  { id: 23, parents: [, 106], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 24, parents: [6, 16], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 25, parents: [6,106], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },*/
+  { id: 26, parents: [7, 107], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+/*  { id: 27, parents: [, 107], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 28, parents: [7, 17], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 29, parents: [7,107], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },*/
+  { id: 30, parents: [8, 108], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+/*  { id: 31, parents: [, 108], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 32, parents: [8, 18], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 33, parents: [8,108], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },*/
+  { id: 34, parents: [9, 109], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+/*  { id: 35, parents: [, 109], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 36, parents: [9, 19], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 37, parents: [9,109], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },*/
+  { id: 38, parents: [10, 110], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+/*  { id: 39, parents: [1, 110], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 40, parents: [10, 10], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 41, parents: [10,110], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },*/
+  { id: 42, parents: [11, 111], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },    
+/*  { id: 43, parents: [1, 111], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 44, parents: [11, 11], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 45, parents: [11,111], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },*/
+  { id: 46, parents: [12, 112], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+/*  { id: 47, parents: [1, 112], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 28, parents: [12, 12], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 29, parents: [12,112], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },*/
+  { id: 48, parents: [13, 113], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+/*  { id: 49, parents: [1, 113], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 50, parents: [13, 13], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 51, parents: [13,113], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },*/
+  { id: 52, parents: [14, 114], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+/*  { id: 53, parents: [1, 114], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 54, parents: [14, 14], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 55, parents: [14,114], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },*/
+  { id: 56, parents: [15, 115], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+/*  { id: 57, parents: [1, 115], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 58, parents: [15, 15], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 59, parents: [15,115], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },*/
+  { id: 60, parents: [16, 116], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+/*  { id: 61, parents: [1, 116], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 62, parents: [16, 16], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 63, parents: [16,116], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },*/
+  { id: 64, parents: [17, 117], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+/*  { id: 65, parents: [1, 117], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 66, parents: [17, 17], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 67, parents: [17,117], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },*/
+  { id: 68, parents: [18, 118], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+/*  { id: 69, parents: [1, 118], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 70, parents: [18, 18], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 71, parents: [18,118], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },*/
+  { id: 72, parents: [19, 119], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+/*  { id: 73, parents: [1, 119], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 74, parents: [19, 19], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 75, parents: [19,119], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },*/
+  { id: 76, parents: [20, 120], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+/*  { id: 77, parents: [2, 120], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 78, parents: [20, 10], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 79, parents: [20,120], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },*/
+  { id: 80, parents: [21, 121], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+/*  { id: 81, parents: [2, 121], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 82, parents: [21, 11], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },
+  { id: 83, parents: [21,121], title: "Surname Name",  description: "Description", image: "photos/v.png", itemTitleColor: "#c8e4fe" },*/
+
+
+
+
+];
+
+
+
